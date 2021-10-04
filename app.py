926344d176e58657ec19616d8311d7d1f68f4c90
Christian Gallo <contato.gallodev@gmail.com>
2021-10-04 20:01:23 -0300

    feat(App): add message var

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+message = 'Hello World'
 @app.route("/")
 def pagina_inicial():
-    return "Hello World"
+    return message
 
 if __name__ == '__main__':
     app.run(debug=True)
